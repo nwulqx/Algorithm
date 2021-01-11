41bee054e0c40a1f807a00cea83c265a0cf1c2c1
LQX-NWU <nwulqx@foxmail.com>
2021-01-11 23:12:42 +0800

    feat: add 144 94 145 102

---
+++ LeetCode/medium/144.BinaryTreePreorderTraversal/BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+   public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        preOrder(root, result);
+        return result;
+    }
+    void preOrder(TreeNode* root, vector<int>& result) {
+        if (root == NULL) {
+            return;
+        }
+        result.push_back(root->val);
+        preOrder(root->left, result);
+        preOrder(root->right, result);
+    }
+};
---
+++ LeetCode/medium/94.BinaryTreeInorderTraversal/BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+   public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        inOrder(root, result);
+        return result;
+    }
+    void inOrder(TreeNode* root, vector<int>& result) {
+        if (root == NULL) {
+            return;
+        }
+        inOrder(root->left, result);
+        result.push_back(root->val);
+        inOrder(root->right, result);
+    }
+};
