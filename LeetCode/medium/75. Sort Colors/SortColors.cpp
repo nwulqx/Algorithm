524d492db90ba8c5242a8ecadaf0583881961be6
LQX-NWU <nwulqx@foxmail.com>
2021-01-19 22:35:32 +0800

    feat: array 75

---
+++ LeetCode/medium/75. Sort Colors/SortColors.cpp
@@ -0,0 +1,25 @@
+class Solution {
+   public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 2)
+            return;
+        //双指针
+        int p = 0, q = n - 1;
+        for (int i = 0; i <= q; ++i) {
+            if (nums[i] == 0) {
+                nums[i] = nums[p];
+                nums[p] = 0;
+                ++p;
+            }
+            if (nums[i] == 2) {
+                nums[i] = nums[q];
+                nums[q] = 2;
+                --q;
+                if (nums[i] != 1)
+                    --i;
+            }
+        }
+        return;
+    }
+};
