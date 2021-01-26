5d8d0b9f1566809794c70ab1d3c811a13d0b7b5c
LQX-NWU <nwulqx@foxmail.com>
2021-01-26 21:47:10 +0800

    feat: 11

---
+++ LeetCode/medium/11. Container With Most Water/ContainerWithMostWater.cpp
@@ -0,0 +1,16 @@
+class Solution {
+   public:
+    int maxArea(vector<int>& height) {
+        int low = 0, high = height.size() - 1;
+        int area = 0;
+        while (low < high) {
+            area = max(area, min(height[low], height[high]) * (high - low));
+            if (height[low] < height[high]) {
+                low++;
+            } else {
+                high--;
+            }
+        }
+        return area;
+    }
+};
