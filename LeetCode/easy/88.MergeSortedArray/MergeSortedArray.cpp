752146a451346ac7f4eead81e74630022eb67b9a
LQX-NWU <nwulqx@foxmail.com>
2021-01-28 22:15:28 +0800

    add  88

---
+++ LeetCode/easy/88.MergeSortedArray/MergeSortedArray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+   public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = nums1.size() - 1;
+        m--, n--;
+        while (i >= 0) {
+            while (m >= 0 || n >= 0) {
+                if (m < 0) {
+                    nums1[i--] = nums2[n--];
+                } else if (n < 0) {
+                    nums1[i--] = nums1[m--];
+                } else if (nums1[m] > nums2[n]) {
+                    nums1[i--] = nums1[m--];
+                } else {
+                    nums1[i--] = nums2[n--];
+                }
+            }
+        }
+    }
+};
